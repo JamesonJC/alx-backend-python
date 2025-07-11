dfb0a5000220ed9d62c4e2b6a7d39ad99478f5b5
JamesonJC <41649261+JamesonJC@users.noreply.github.com>
2025-07-11 10:11:58 +0000

    0. Logging database Queries

---
+++ python-generators-0x01/0-log_queries.py
@@ -0,0 +1,29 @@
+
+import sqlite3
+#import functools
+
+#### decorator to log SQL queries
+
+def log_queries(func):
+    def wrapper(*args, **kwargs):
+        # Get the query from the first argument or kwargs
+        # query = kwargs.get('query', '')
+        print(f"Executing query: {kwargs}")
+        return func(*args, **kwargs)
+    
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    # print(f"Executing query: {query}")
+    # for row in results:
+    #     print(row)
+    # conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
