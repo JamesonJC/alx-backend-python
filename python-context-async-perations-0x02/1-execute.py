c6a620837c5b80c815a81e93291e38f1f511441e
JamesonJC <41649261+JamesonJC@users.noreply.github.com>
2025-07-13 16:37:45 +0000

    decorators, context managers and asyncronouse functions"

--- python-context-async-perations-0x02/1-execute.py
+++ python-context-async-perations-0x02/1-execute.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import sqlite3
-
+from contextlib import contextmanager
 class ExecuteQuery:
     def __init__(self, db_name, query, params=None):
         self.db_name = db_name
@@ -21,3 +21,12 @@ class ExecuteQuery:
     def execute(self):
         self.cursor.execute(self.query, self.params)
         return self.cursor.fetchall()
+    
+def main():
+    query = "SELECT * FROM users WHERE age > ?"
+    params = (25,)
+
+    with ExecuteQuery("users.db", query, params) as executor:
+        results = executor.execute()
+        for row in results:
+            print(row)
