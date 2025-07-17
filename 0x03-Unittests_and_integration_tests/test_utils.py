f5fea67656e8aa4b9d4018edda41580796783e2f
JamesonJC <41649261+JamesonJC@users.noreply.github.com>
2025-07-17 12:19:23 +0000

    2. Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,11 +7,11 @@ from typing import Dict, Tuple, Union
 from unittest.mock import patch, Mock
 from parameterized import parameterized
 
-from utils import * #(
-    #access_nested_map,
-    #get_json,
+from utils import (
+    access_nested_map,
+    get_json,
     #memoize,
-#)
+)
 
 
 class TestAccessNestedMap(unittest.TestCase):
