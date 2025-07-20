9c809f398165c7100345df4d70a037143029ee55
JamesonJC <41649261+JamesonJC@users.noreply.github.com>
2025-07-20 19:13:18 +0000

    2. Create serializers to define the many to many relationships

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -3,17 +3,28 @@ from .models import User, Message, Conversation
 
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ["user_id", "first_name", "last_name", "email", "phone_number", "role", "created_at"]
+        fields = ["user_id", "first_name", "last_name", "full_name", "email", "phone_number", "role", "created_at"]
+
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
 
 
 class MessageSerializer(serializers.ModelSerializer):
+    message_body = serializers.CharField(max_length=1000)
     sender = UserSerializer(read_only=True)
 
     class Meta:
         model = Message
         fields = ["message_id", "sender", "message_body", "sent_at"]
+
+    def validate_message_body(self, value):
+        if len(value.strip()) == 0:
+            raise serializers.ValidationError("Message body cannot be empty or just whitespace.")
+        return value
 
 
 class ConversationSerializer(serializers.ModelSerializer):
@@ -23,4 +34,4 @@ class ConversationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conversation
         fields = ["conversation_id", "participants", "messages", "created_at"]
-    
+        
