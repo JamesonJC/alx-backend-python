ff8b7865cc4dbd2106a1e1ceade15db94cc44374
JamesonJC <41649261+JamesonJC@users.noreply.github.com>
2025-07-20 16:09:16 +0000

    0. Project set up

---
+++ messaging_app/chats/urls.py
@@ -0,0 +1,8 @@
+# chats/urls.py
+
+from django.urls import path
+from .views import MessageListCreateView
+
+urlpatterns = [
+    path('messages/', MessageListCreateView.as_view(), name='message-list-create'),
+]
