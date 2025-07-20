bf05e6d6c1a466e8b4056ea1be878806bf563414
JamesonJC <41649261+JamesonJC@users.noreply.github.com>
2025-07-20 19:17:30 +0000

    3. Build api endpoints with views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,15 +1,16 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
-from .models import Conversation, Message, User
+from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['participants__first_name', 'participants__last_name']
 
     def perform_create(self, serializer):
-        # This assumes the request user is already authenticated
         conversation = serializer.save()
         conversation.participants.add(self.request.user)
         conversation.save()
@@ -18,6 +19,8 @@ class ConversationViewSet(viewsets.ModelViewSet):
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['message_body']
 
     def perform_create(self, serializer):
         serializer.save(sender=self.request.user)
