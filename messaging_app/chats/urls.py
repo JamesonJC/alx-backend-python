98f13f020136231ee18bdfefeef0ed6bda5ad15e
JamesonJC <41649261+JamesonJC@users.noreply.github.com>
2025-07-20 19:10:20 +0000

    2. Create serializers to define the many to many relationships

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,12 @@
 # chats/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ConversationViewSet, MessageViewSet
 
-from django.urls import path
-from .views import MessageListCreateView
+router = DefaultRouter()
+router.register(r'conversations', ConversationViewSet)
+router.register(r'messages', MessageViewSet)
 
 urlpatterns = [
-    path('messages/', MessageListCreateView.as_view(), name='message-list-create'),
+    path('', include(router.urls)),
 ]
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Conversation, Message, User
+from .serializers import ConversationSerializer, MessageSerializer
 
-# Create your views here.
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    def perform_create(self, serializer):
+        # This assumes the request user is already authenticated
+        conversation = serializer.save()
+        conversation.participants.add(self.request.user)
+        conversation.save()
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
